ae697ff8fad5a171ffa47e16bd770d27f075d7e5
Jair <jrevilla492@gmail.com>
2025-09-01 13:24:57 -0500

    init

--- settings.php
+++ settings.php
@@ -1,18 +1,21 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
+/** @var TYPE_NAME $hassiteconfig */
 if ($hassiteconfig) {
+    // Agregar al menú de reportes
+    /** @var TYPE_NAME $ADMIN */
+    if ($ADMIN->locate('reports')) {
+        $ADMIN->add('reports', new admin_category('cadreports',
+            get_string('pluginname', 'local_cadreports')));
 
-    // Crear categoría principal CAD
-    $ADMIN->add('reports', new admin_category('cadreports',
-        get_string('pluginname', 'local_cadreports')));
+        // Reporte de accesos
+        $ADMIN->add('cadreports', new admin_externalpage(
+            'cadreports_access',
+            get_string('access_sessions', 'local_cadreports'),
+            new moodle_url('/local/cadreports/reports/access_report.php'),
+            ['local/cadreports:view', 'local/cadreports:viewall']
+        ));
 
-    // Agregar página de reportes
-    $ADMIN->add('cadreports', new admin_externalpage(
-        'local_cadreports',
-        get_string('reports_menu', 'local_cadreports'),
-        new moodle_url('/local/cadreports/index.php'),
-        'local/cadreports:view'
-    ));
+    }
 }
-?>
